7a5ddbc662b7b182cdd721f53fa0ae469a10bfc6
elgrande73 <elgrande.github@gmail.com>
2026-07-31 21:04:12 +0300

    Remove unused Firebase entries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,6 @@ secrets {
 // This makes it easy to add them back in the future if needed.
 dependencies {
   implementation(platform(libs.androidx.compose.bom))
-  implementation(platform(libs.firebase.bom))
   // implementation(libs.accompanist.permissions)
   implementation(libs.androidx.activity.compose)
   // implementation(libs.androidx.camera.camera2)
@@ -74,7 +73,6 @@ dependencies {
   implementation(libs.androidx.room.runtime)
   // implementation(libs.coil.compose)
   implementation(libs.converter.moshi)
-  // implementation(libs.firebase.ai)
   implementation(libs.kotlinx.coroutines.android)
   implementation(libs.kotlinx.coroutines.core)
   implementation(libs.logging.interceptor)
