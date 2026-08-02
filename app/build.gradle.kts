a0584dd666b0558bf28a81f0be15409b884d16cd
elgrande73 <elgrande.github@gmail.com>
2026-08-02 21:48:59 +0300

    Make release signing optional for CI builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,27 +35,32 @@ android {
         includeInBundle = false
   }
   
-   signingConfigs {
+  signingConfigs {
+    if (keystorePropertiesFile.exists()) {
         create("release") {
             storeFile = file(keystoreProperties["storeFile"] as String)
             storePassword = keystoreProperties["storePassword"] as String
             keyAlias = keystoreProperties["keyAlias"] as String
             keyPassword = keystoreProperties["keyPassword"] as String
         }
+    }
   }
 
 
   buildTypes {
-        release {
+    release {
+        if (keystorePropertiesFile.exists()) {
             signingConfig = signingConfigs.getByName("release")
-
-            isCrunchPngs = false
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
+
+        isCrunchPngs = false
+        isMinifyEnabled = false
+        proguardFiles(
+            getDefaultProguardFile("proguard-android-optimize.txt"),
+            "proguard-rules.pro"
+        )
+    }
+
     debug {
     }
   }
